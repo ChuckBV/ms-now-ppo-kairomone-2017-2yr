0dcf71426fe1acc76b09190ee57c9d260b7d5737
ChuckBV <c_s_burks@yahoo.com>
2019-08-12 11:25:13 -0700

    tweak

--- scripts/script5.R
+++ scripts/script5.R
@@ -24,28 +24,21 @@ library(userfriendlyscience)
 se <- function(number){ 
   sd(number, na.rm = TRUE)/sqrt(sum(!is.na(number), na.rm = TRUE))
 }
-library(stringr)
-numextract <- function(string){ 
-  str_extract(string, "\\-*\\d+\\.*\\d*")
-} 
-lttrextract <-function(string){
-  str_extract(string, "\\-*\\D+\\.*\\D*")
-} 
 
 #== 1. Count data, 2019 trap test ======================================
 
-y19_delta <- read_csv("./data/Y19_delta.csv")
-y19_delta <- y19_delta[ ,3:8] 
+delta <- read_csv("./data/Y19_delta.csv")
+delta <- delta[ ,3:8] # Drop Experiment and Location contants
 
 # Make Treatment an ordered factor with the desired order
-y19_delta <- y19_delta %>% 
+delta <- delta %>% 
   mutate(Treatment = factor(Treatment, 
                             levels = c("WingPhero","WingPpo","WingCombo","DeltaPpo","DeltaCombo","ModPpo","ModCombo")))
-levels(y19_delta$Treatment)
+levels(delta$Treatment)
 
-y19_delta$StartDate <- as.Date(y19_delta$StartDate)
-y19_delta$EndDate <- as.Date(y19_delta$EndDate)
-strt_dats <- unique(y19_delta$StartDate)
-end_dats <- unique(y19_delta$EndDate)
+delta$StartDate <- as.Date(delta$StartDate)
+delta$EndDate <- as.Date(delta$EndDate)
+strt_dats <- unique(delta$StartDate)
+end_dats <- unique(delta$EndDate)
 
 nmbr_wks <- as.numeric(sum(end_dats - strt_dats)/7)
