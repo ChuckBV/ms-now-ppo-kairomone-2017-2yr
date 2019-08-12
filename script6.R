efaefd9a0549f464cd9133936d9c4af9bbc8e6b6
ChuckBV <c_s_burks@yahoo.com>
2019-08-12 16:13:04 -0700

    Start script6.R

---
+++ script6.R
@@ -0,0 +1,26 @@
+#============================================================================
+# script6.R
+#
+# Sex ratios
+#
+# Parts
+# 1. Import data set to data frame (line 32)
+# 2. Characterize number of NAs, sampling dates, and weekly phenology 
+#    (line 65)
+# 3. Pool data across dates, output for SAS, and obtain a table of 
+#    mean and SE
+#
+#============================================================================
+
+# load libaries
+library(tidyverse)
+library(lubridate)
+library(DescTools)
+library(viridis)
+library(multcompView)
+library(userfriendlyscience)
+
+# load functions
+se <- function(number){ 
+  sd(number, na.rm = TRUE)/sqrt(sum(!is.na(number), na.rm = TRUE))
+}
