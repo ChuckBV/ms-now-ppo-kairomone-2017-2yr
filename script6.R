a1bf753eaa4e57605cdb57aebd8174b90ebf1b13
ChuckBV <c_s_burks@yahoo.com>
2019-08-12 20:16:27 -0700

    Start on script6, sex ratio in PPO

--- script6.R
+++ script6.R
@@ -24,3 +24,27 @@ library(userfriendlyscience)
 se <- function(number){ 
   sd(number, na.rm = TRUE)/sqrt(sum(!is.na(number), na.rm = TRUE))
 }
+
+### Use script1 to load each of the data sets into the global environment
+### These include
+###  - y17_lures
+###  - y17_sexes
+###  - y18_july
+###  - y18_june
+###  - y18_lures
+###  - y10_delta
+
+y17_sexes
+# A tibble: 2,224 x 12
+#    TrtID TrtCode Treatment Crop  MD    Rep    Trap SiteID  Week Date       Sex    Count
+#    <dbl> <chr>   <fct>     <fct> <fct> <fct> <dbl>  <dbl> <dbl> <date>     <fct>  <dbl>
+#  1     3 C       PpoCombo  Alm   Yes   1       111      1    24 2017-06-15 Female     5
+#  2     3 C       PpoCombo  Alm   Yes   1       111      1    24 2017-06-15 Male       8
+#  3     3 C       PpoCombo  Alm   Yes   1       111      1    25 2017-06-23 Female     0
+#  4     3 C       PpoCombo  Alm   Yes   1       111      1    25 2017-06-23 Male      10
+#  5     3 C       PpoCombo  Alm   Yes   1       111      1    27 2017-07-05 Female     5
+#  6     3 C       PpoCombo  Alm   Yes   1       111      1    27 2017-07-05 Male      18
+#  7     3 C       PpoCombo  Alm   Yes   1       111      1    28 2017-07-14 Female     4
+#  8     3 C       PpoCombo  Alm   Yes   1       111      1    28 2017-07-14 Male       5
+#  9     3 C       PpoCombo  Alm   Yes   1       111      1    29 2017-07-18 Female     2
+# 10     3 C       PpoCombo  Alm   Yes   1       111      1    29 2017-07-18 Male       9
